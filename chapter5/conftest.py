2e37261ed25b63fe2825f437400e5f5b74249777
wooyoung.choi <wooyoung.choi@wonderwall.kr>
2025-09-24 13:02:33 +0900

    chapter5

--- chapter5/conftest.py
+++ chapter5/conftest.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from playwright.sync_api import Playwright
 
@@ -43,4 +44,55 @@ def function_page(playwright:Playwright):
     page.goto("https://study.wooyoung.site/")
     yield page
     browser.close()
+def do_login(context, cookie_path: str):
+    page = context.new_page()
+    page.goto("https://store-qa.frommyarti.com/arti")
 
+    page.wait_for_url("https://store-qa.frommyarti.com/arti")
+    page.wait_for_load_state('networkidle')
+
+
+    menu_name = '전체메뉴'
+
+    header = page.locator('header')
+    menu_btn = header.get_by_role('button', name=menu_name)
+    menu_btn.click()
+
+    move_login_page_btn = page.locator('a', has_text='로그인 후 이용해주세요.')
+    move_login_page_btn.click()
+
+    id_input = page.locator("input[placeholder='example@email.com']")
+    id_input.type("it.test+VHWyBU@wonderwall.kr")
+
+    pw_input = page.locator("input[placeholder='비밀번호']")
+    pw_input.type("qwer1234!")
+
+    login_btn = page.locator('span', has_text='로그인')
+    login_btn.click()
+
+    time.sleep(10)
+    context.storage_state(path=cookie_path)
+
+    return context
+@pytest.fixture(scope="session")
+def session_store_page(playwright:Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+
+    do_login(context, 'cookie.json')
+
+    context.close()
+    browser.close()
+
+    yield 'cookie.json'
+
+
+@pytest.fixture
+def function_store_page(playwright:Playwright,session_store_page):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state=session_store_page)
+    page = context.new_page()
+
+    page.goto("https://store-qa.frommyarti.com/arti")
+    yield page
+    browser.close()
---
+++ chapter5/test_cookie.py
@@ -0,0 +1,5 @@
+import time
+
+
+def test_cookie(function_store_page):
+    time.sleep(15)
